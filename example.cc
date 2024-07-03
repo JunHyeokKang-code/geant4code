677897f10547834b3c943387e82cc43e078634c3
JunHyeokKang-code <christopher123@naver.com>
2024-07-03 13:28:25 +0900

    Update example.cc

--- example.cc
+++ example.cc
@@ -15,7 +15,7 @@
 
 int main(int argc, char** argv)
 {
-  G4RunManager* runManager = new G4RunManager;
+  G4RunManager* runManager = new G4RunManager; // run manager 객체 생성 , runmanager 는 전체 흐름 관
   
   G4VModularPhysicsList* physicsList = new QGSP_BERT; //physics 모델 , 원하는 시뮬레이션에 따라 변경 가능 , QSP + BERT 모델 
   //runManager -> SetUserInitialization(physicsList);
@@ -31,7 +31,8 @@ int main(int argc, char** argv)
   visManager -> Initialize(); //코드 실행을 위한 초기화
   
 //다음은 ui 실행 옵션
-  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+  G4UImanager* UImanager = G4UImanager::GetUIpointer(); // 인터페이스 설정프로그램이 인자를 받아 실행될 경우 해당 인자(매크로 파일)를 실행
+                                                        // 인자가 없으면 기본 매크로 파일 vis.mac을 실행하고 인터랙티브 세션을 시작 
   if (argc != 1) 
   {
     G4String command = "/control/execute "; 
@@ -46,7 +47,7 @@ int main(int argc, char** argv)
     delete ui;
   }
 
-  delete visManager;
+  delete visManager; // 시뮬레이션이 끝난 후 visManager와 runManager를 삭제하여 메모리를 정리
   delete runManager;
 
   return 0;
